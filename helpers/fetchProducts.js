ddc4657fab5e5095436b12015a69bcf8b7ba87f7
AlissaMaximo <alissahiratamaximo@gmail.com>
2022-01-27 02:37:03 -0300

    Requisitos 1, 2, 3 e 8 completos.

--- helpers/fetchProducts.js
+++ helpers/fetchProducts.js
@@ -1,6 +1,12 @@
-const fetchProducts = () => fetch('https://api.mercadolibre.com/sites/MLB/search?q=$computador')
+const fetchProducts = async (param) => {
+  try {
+  return await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${param}`)
   .then((response) => response.json())
   .then((object) => object);
+  } catch (error) {
+    throw new Error('You must provide an url');
+  }
+}
 
 if (typeof module !== 'undefined') {
   module.exports = {
--- script.js
+++ script.js
@@ -30,6 +30,8 @@ function getSkuFromProductItem(item) {
 
 function cartItemClickListener(event) {
   // coloque seu código aqui
+  const cartList = document.querySelector('.cart__items');
+  event.target.remove();
 }
 
 function createCartItemElement({ sku, name, salePrice }) {
@@ -40,16 +42,51 @@ function createCartItemElement({ sku, name, salePrice }) {
   return li;
 }
 
-async function init() {
+async function createProductsList() {
   const data = await fetchProducts();
   const { results } = data;
   const items = document.querySelector('.items');
 
   results.forEach((result) => {
-    const { id, title, thumbnail } = result;
-    const elementProduct = createProductItemElement({ id, title, thumbnail });
+    const sku = result.id;
+    const name = result.title;
+    const image = result.thumbnail;
+    const elementProduct = createProductItemElement({ sku, name, image });
     items.appendChild(elementProduct);
   });
 }
 
-window.onload = () => { init(); };
+async function getItemSku(itemSku) {
+  const selectedItem = await fetchItem(itemSku);
+  const { id: sku, title: name, price: salePrice } = selectedItem;
+  const cartItem = createCartItemElement({ sku, name, salePrice });
+  const cartList = document.querySelector('.cart__items');
+  cartList.appendChild(cartItem);
+}
+
+function addToCart() {
+  const allAddButtons = document.querySelectorAll('.item__add');
+
+  allAddButtons.forEach((addButton) => {
+    addButton.addEventListener('click', (e) => {
+      const elementParent = e.target.parentNode;
+      const itemSku = getSkuFromProductItem(elementParent);
+      getItemSku(itemSku);
+    })
+  })
+}
+
+window.onload = async () => {
+  await createProductsList();
+  addToCart();
+};
+
+/*
+REFERÊNCIAS
+Requisito 1: Tonn me auxiliou a mostrar as informações na página.
+Requisito 2 e 8: Lilian Azevedo me auxiliou a entender o que algumas funções realizam, se deixasse tudo no init() o Linter iria apontar erro, que é necessário 5 its e não 5 expects, e no geral.
+Requisito 8: https://jestjs.io/docs/expect#tohavebeencalled
+https://jestjs.io/docs/expect
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/catch
+
+*/
